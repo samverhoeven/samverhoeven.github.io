4a6de79243e3d968f93246dc8cabc1b0a558fa8b
samverhoeven <sam_verhoeven3@hotmail.com>
2015-10-06 12:07:28 +0200

    Intro boven header

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,23 +1,46 @@
 function windowLoad() {
     var eMenu = document.querySelectorAll("#menu ul li a");
-    
+    var eTop = document.querySelectorAll(".top");
+    var eIntro = document.getElementById("menuLink");
+
     for (i = 0; i < eMenu.length; i++) {
         eMenu[i].addEventListener("click", function (e) {
+            e.preventDefault();
+            scrollTo(this);
+        });
+    }
+    for (i = 0; i < eTop.length; i++) {
+        eTop[i].addEventListener("click", function (e) {
             e.preventDefault();
             scrollTo(this);
             console.log(this);
         });
     }
+    eIntro.addEventListener("click", function (e) {
+        e.preventDefault();
+        scrollTo(this);
+        console.log(this);
+    });
+    eIntro.addEventListener("mouseover",function(e){
+        e.preventDefault();
+        linkExpand(this);
+    });
+    eIntro.addEventListener("mouseout",function(e){
+        e.preventDefault();
+        linkDeflate(this);
+    });
 }
 
 function scrollTo(elem) {
-    
-    var sId = elem.id;
-    sId = sId.replace("Link","");
-    console.log(sId);
-    var eContainer = document.getElementById(sId);
-    console.log(eContainer);
-    window.scroll(0,findPos(eContainer));
+    if (elem.className === "top") {
+        console.log(elem.className);
+        window.scroll(0, 0);
+    } else {
+        var sId = elem.id;
+        sId = sId.replace("Link", "");
+        var eContainer = document.getElementById(sId);
+        window.scroll(0, findPos(eContainer));
+    }
 }
 
 function findPos(elem) {
@@ -26,7 +49,17 @@ function findPos(elem) {
         do {
             curtop += elem.offsetTop;
         } while (elem = elem.offsetParent);
-    return [curtop];
+        return [curtop];
     }
+}
+
+function linkExpand(elem){
+    var eParent = elem.parentNode;
+    eParent.style.background = "#4863A0";
+}
+
+function linkDeflate(elem){
+    var eParent = elem.parentNode;
+    eParent.style.background = "black";
 }
 
