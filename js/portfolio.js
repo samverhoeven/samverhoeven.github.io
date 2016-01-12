0af095f3d786d5d9e9d3711a57533c55774be42d
samverhoeven <sam_verhoeven3@hotmail.com>
2016-01-12 12:14:47 +0100

    positionering + animatie skillbars

--- js/portfolio.js
+++ js/portfolio.js
@@ -27,7 +27,6 @@ function windowLoad() {
 
     if ($(window).width() > 749) {
         $("body").FancyIndex({//fancy index met positieindicatie
-            firstOnly: true,
             scrollToDuration: 500
         });
     }
@@ -37,34 +36,60 @@ function windowLoad() {
          section: "section"
          });*/
         /*var scrollorama = $.scrollorama({//animatie per scrollblock
-            blocks: "section",
-            enablePin: false
-        });
+         blocks: "section",
+         enablePin: false
+         });
+         
+         //#info scrollanimaties
+         scrollorama.animate("#info .heading2", {duration: 500, delay: 100, property: "left", start: -300, end: 0, easing: "easeInOutCubic"});
+         scrollorama.animate("#info .heading2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
+         //scrollorama.animate("#info .tekst", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
+         scrollorama.animate("#info .tekst", {duration: 500, delay: 100, property: "top", start: 300, end: 0, easing: "easeInOutCubic"});
+         
+         //#skills scrollanimaties
+         scrollorama.animate("#skills .heading2", {duration: 500, delay: 100, property: "right", start: -300, end: 0, easing: "easeInOutCubic"});
+         scrollorama.animate("#skills .heading2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
+         
+         //#projecten scrollanimaties
+         scrollorama.animate("#projecten .heading2", {duration: 500, delay: 100, property: "left", start: -300, end: 0, easing: "easeInOutCubic"});
+         scrollorama.animate("#projecten .heading2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
+         scrollorama.animate("#projecten .tekst", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
+         scrollorama.animate("#projecten .tekst", {duration: 500, delay: 100, property: "left", start: 500, end: 0, easing: "easeInOutCubic"});
+         
+         //#contact scrollanimaties
+         //scrollorama.animate("#contact .h2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
+         scrollorama.animate("#contact .heading2", {duration: 500, delay: 100, property: "left", start: -300, end: 0, easing: "easeInOutCubic"});
+         scrollorama.animate("#contact form", {duration: 500, delay: 100, property: "opacity", start: 0.5, end: 1, easing: "easeInOutCubic"});
+         scrollorama.animate("#contact form", {duration: 500, delay: 100, property: "top", start: 200, end: 0, easing: "easeInOutCubic"});*/
 
-        //#info scrollanimaties
-        scrollorama.animate("#info .heading2", {duration: 500, delay: 100, property: "left", start: -300, end: 0, easing: "easeInOutCubic"});
-        scrollorama.animate("#info .heading2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
-        //scrollorama.animate("#info .tekst", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
-        scrollorama.animate("#info .tekst", {duration: 500, delay: 100, property: "top", start: 300, end: 0, easing: "easeInOutCubic"});
-
-        //#skills scrollanimaties
-        scrollorama.animate("#skills .heading2", {duration: 500, delay: 100, property: "right", start: -300, end: 0, easing: "easeInOutCubic"});
-        scrollorama.animate("#skills .heading2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
-
-        //#projecten scrollanimaties
-        scrollorama.animate("#projecten .heading2", {duration: 500, delay: 100, property: "left", start: -300, end: 0, easing: "easeInOutCubic"});
-        scrollorama.animate("#projecten .heading2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
-        scrollorama.animate("#projecten .tekst", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
-        scrollorama.animate("#projecten .tekst", {duration: 500, delay: 100, property: "left", start: 500, end: 0, easing: "easeInOutCubic"});
-
-        //#contact scrollanimaties
-        //scrollorama.animate("#contact .h2", {duration: 500, delay: 100, property: "opacity", start: 0, end: 1, easing: "easeInOutCubic"});
-        scrollorama.animate("#contact .heading2", {duration: 500, delay: 100, property: "left", start: -300, end: 0, easing: "easeInOutCubic"});
-        scrollorama.animate("#contact form", {duration: 500, delay: 100, property: "opacity", start: 0.5, end: 1, easing: "easeInOutCubic"});
-        scrollorama.animate("#contact form", {duration: 500, delay: 100, property: "top", start: 200, end: 0, easing: "easeInOutCubic"});*/
-        
         new ScrollFlow();
-        
+
+        $(window).on("scroll", function () {
+            var opacitystr = $("#skills .heading2").css("webkitFilter");
+            var regExp = /\(([^)]+)\)/;
+            var modopacitystr = regExp.exec(opacitystr);
+            modopacitystr = modopacitystr[0].substring(1).slice(0, -1);
+            console.log(modopacitystr);
+
+            if (modopacitystr >= 0.9) {
+                console.log("test skills heading opacity");
+                $("#skillbar1").animate({width: "95%"}, 1000);
+                $("#skillbar2").animate({width: "95%"}, 1000);
+                $("#skillbar3").animate({width: "90%"}, 1000);
+                $("#skillbar4").animate({width: "90%"}, 1000);
+                $("#skillbar5").animate({width: "80%"}, 1000);
+                $("#skillbar6").animate({width: "70%"}, 1000);
+                $("#skillbar7").animate({width: "70%"}, 1000);
+                $("#skillbar8").animate({width: "60%"}, 1000);
+                $("#skillbar9").animate({width: "50%"}, 1000);
+            } else {
+                $(".skillbar").animate({width: "0%"}, 500);
+            }
+        });
+        $(window).on('scroll', function (e) {
+            console.log(e.type + '-event was 250ms not triggered');
+        }, 250);
+
     } else {
         $("#esKju-fancyIndex li").addClass("active");
     }
@@ -81,7 +106,7 @@ function windowLoad() {
                 "</div>" +
                 "</div>");
 
-        var bg = jQuery("#intro_back");
+        var bg = $("#intro_back");
 
         bg.css("background-attachment", "scroll");
 
