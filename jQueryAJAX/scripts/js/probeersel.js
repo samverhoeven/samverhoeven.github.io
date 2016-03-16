f9b07a1bad2f9a3da5414079767c45ae52aee95e
samverhoeven <sam_verhoeven3@hotmail.com>
2016-03-16 15:08:22 +0100

    jQueryAJAX project styling

--- jQueryAJAX/scripts/js/probeersel.js
+++ jQueryAJAX/scripts/js/probeersel.js
@@ -3,7 +3,7 @@ $(window).load(function () {
     var access_token_fb = "438252053041957|IHjpcd9zGhWesX5EuwO2_77pOH8";
     var access_token_ig = "352219268.5ff0997.f0b46231d61849009717f1495bef453f";
     var fields = "message,created_time,picture";
-    var since = "2015-12-01";
+    var since = "2016-03-01";
 
     $.when(getJSONResult("https://graph.facebook.com/cocacolabelgium/feed?fields=" + fields + "&since=" + since + "&access_token=" + access_token_fb),
             getJSONResult("https://graph.facebook.com/lipton/feed?fields=" + fields + "&since=" + since + "&access_token=" + access_token_fb),
@@ -13,20 +13,21 @@ $(window).load(function () {
                 json2 = json2[0].data;
                 json3 = json3[0].data;
                 var json = json1.concat(json2).concat(json3);
-                console.log(json);
                 json.sort(custom_sort);
                 $.each(json, function (i, value) {
                     var time = value.created_time;
                     var inhoud = "";
-                    inhoud += "<div class='post'>" + i + 1 + ": " + value.created_time;
-                    if (value.message != null) {
-                        inhoud += " " + value.message;
-                    } else {
-                        inhoud += " no message";
-                    }
+                    inhoud += "<div class='post'><div class='datum'>" + i + 1 + ": " + value.created_time + "</div><div class='bericht'>";
+
                     if (value.picture != null) {
-                        inhoud += " <img src=" + value.picture + " />";
+                        inhoud += "<img src=" + value.picture + " />";
                     }
+                    if (value.message != null) {
+                        inhoud += value.message + "</div>";
+                    } else {
+                        inhoud += "no message</div>";
+                    }
+
                     inhoud += "</div>";
                     $("#inhoud").append(inhoud);
                 });
